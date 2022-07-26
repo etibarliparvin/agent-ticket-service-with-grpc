a5f6c399c375d10b1d891f9a9f169c7276cecf3a
etibarliparvin <etibarliparvin@gmail.com>
2022-07-26 16:08:23 +0400

    .

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = Byte.parseByte(Integer.toString(12));
+        System.out.println(b);
+    }
+}
